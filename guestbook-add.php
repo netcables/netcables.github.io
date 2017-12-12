8f5ed8dd3e8c00b313ef20390502b31977d578e9
Antonio Carmona <adcarmona98@gmail.com>
2017-12-11 18:23:22 -0800

    Fixed guestbook
    Fixed guestbook by adding lastName

--- guestbook-add.php
+++ guestbook-add.php
@@ -1,8 +1,26 @@
-<html>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Antonio Carmona</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Favicon -->
+  <link rel=icon href="/assets/img/favicon.ico">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/assets/css/site.css">
+</head>
+
 <body>
 
-Welcome <?php echo $_POST["firstName"]; ?><br>
+
+Welcome <?php echo $_POST["firstName"] $_POST["lastName"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
 
 </body>
+
 </html>
